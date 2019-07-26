9c1729098dde103ad276a244374fada77ed51a26
s-heins <sonja.heins@gmail.com>
2019-07-26 15:59:13 +0200

    Logging test outcomes

--- src/otherGenerators.test.ts
+++ src/otherGenerators.test.ts
@@ -1,5 +1,6 @@
 import * as assert from "assert";
 import {otherGeneratorLevelOne} from "./otherGenerators";
+import * as util from "util";
 
 describe('generators', () => {
     let generator: Generator;
@@ -10,8 +11,19 @@ describe('generators', () => {
 
 
     it('calls the last function', async () => {
-        const genTwo: Generator = generator.next().value;
-        const genThree: Generator = genTwo.next().value;
-        assert.strictEqual(genThree.next().value, 6);
+        console.log(util.inspect(generator), 'gen');
+        const genTwo = generator.next();
+        console.log(genTwo.value, genTwo.done, 'gen 2');
+        const genThree = genTwo.value.next();
+        console.log(genThree.value, genThree.done, 'gen 3');
+        const outcome = genThree.value.next();
+        console.log(outcome.value, outcome.done, 'outcome');
+        const outcome3 = genThree.value.next();
+        console.log(outcome3.value, outcome3.done, 'outcome3');
+        const outcome2 = genTwo.value.next();
+        console.log(outcome2.value, outcome2.done, 'outcome2');
+        const outcome1 = generator.next();
+        console.log(outcome1.value, outcome1.done, 'outcome1');
+        assert.strictEqual(outcome.value, 6);
     });
 });
